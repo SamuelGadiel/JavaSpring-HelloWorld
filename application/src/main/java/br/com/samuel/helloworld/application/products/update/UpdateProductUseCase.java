d00b6a3e58b2c814e75c96319a18dc78eaf17144
SamuelGadiel <samuelgadiel@gmail.com>
2023-12-27 17:45:48 -0300

    Update UpdateProductUseCase.java

--- application/src/main/java/br/com/samuel/helloworld/application/products/update/UpdateProductUseCase.java
+++ application/src/main/java/br/com/samuel/helloworld/application/products/update/UpdateProductUseCase.java
@@ -2,6 +2,6 @@ package br.com.samuel.helloworld.application.products.update;
 
 import br.com.zydon.commons.application.UseCase;
 
-public abstract class UpdateProductUseCase extends UseCase<UpdateProductCommand, UpdateProductOutput> {]
+public abstract class UpdateProductUseCase extends UseCase<UpdateProductCommand, UpdateProductOutput> {
 
 }

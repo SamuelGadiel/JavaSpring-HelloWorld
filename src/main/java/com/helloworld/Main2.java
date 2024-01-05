243fd8fce38c3cd13fb090a959ed39151bedfcf9
SamuelGadiel <samuelgadiel@gmail.com>
2024-01-05 10:06:32 -0300

    Update Main2.java

---
+++ src/main/java/com/helloworld/Main2.java
@@ -0,0 +1,10 @@
+package com.helloworld;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+public class Main2 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main2.class, args);
+    }
+}

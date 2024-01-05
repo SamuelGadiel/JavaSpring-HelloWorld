05342ecd6ae79a21542feb68461b12c4130f9186
SamuelGadiel <samuelgadiel@gmail.com>
2024-01-05 10:12:16 -0300

    Create ProductIDType.java

---
+++ infrastructure/src/main/java/br/com/samuel/helloworld/infrastructure/products/persistence/ProductIDType.java
@@ -0,0 +1,8 @@
+package br.com.samuel.helloworld.infrastructure.products.persistence;
+
+import br.com.samuel.helloworld.domain.products.ProductID;
+import br.com.zydon.commons.infrastructure.persistence.EntityIDType;
+
+public class ProductIDType extends EntityIDType<ProductID> {
+
+}
